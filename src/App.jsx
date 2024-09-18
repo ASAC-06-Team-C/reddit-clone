173a43b13f79729a074a5e8c6b6e6f4f023da288
Kang Seok Hoon <hamer1233@naver.com>
2024-09-19 07:05:39 +0900

    Feat : 게시글 조회 형태 구현 게시글을 조회하는 형태를 구현 : 카드 형태 내부에 마크다운 뷰어 추가 필요

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,14 @@ import reactLogo from '@/assets/react.svg'
 import viteLogo from '/vite.svg'
 import '@/App.css'
 
-import { Button } from './components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-
+import Board from '@/Board'
+import BoardDetail from '@/BoardDetail'
 
 function App() {
-
   return (
     <>
-      <Button>Button</Button>
-      <button>Button2</button>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-
+      <Board />
+      <BoardDetail />
     </>
   )
 }
