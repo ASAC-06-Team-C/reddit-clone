fafb64b2563b71ee08ea525fd740329cd0cf9bed
soleswan <eowls1358@gmail.com>
2024-09-19 23:23:06 +0900

    fit: 1. shadcn select component 추가 2. select component 를 이용한 최초 접근 main page 개발   -> RedditFirstAttachPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@/App.css'
 import RedditReactHeader from '@/RedditReactHeader'
+import RedditFirstAttachPage from '@/RedditFirstAttachPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +9,7 @@ function App() {
   return (
     <>
       <RedditReactHeader />
+      <RedditFirstAttachPage />
     </>
   )
 }
