d2ee05091d4126659a20b0811a7922499c231558
noname <jeonyo85@naver.com>
2024-09-20 09:21:18 +0900

    Comment 주석 제거

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -133,17 +133,3 @@ function Comment() {
 }
 
 export default Comment
-
-//오류해결 https://velog.io/@songyeonji_/React%EB%B2%84%EC%A0%84%EA%B3%BC-toast-ui-%EB%B2%84%EC%A0%84-%EC%98%A4%EB%A5%98
-
-//https://github.com/nhn/tui.editor/blob/master/docs/ko/toolbar.md
-//https://bloodstrawberry.tistory.com/1268
-// https://hyonyong.tistory.com/entry/TOAST-UI-Editor-%EC%82%AC%EC%9A%A9%ED%95%B4%EB%B3%B4%EA%B8%B0#%ED%88%B4%EB%B0%94%20%EC%BB%A4%EC%8A%A4%ED%84%B0%EB%A7%88%EC%9D%B4%EC%A7%95-1
-// 옵션
-// - previewStyle (마크다운 - vertical or tab 옵션)
-// - initialEditType (처음 편집 타입 설정 wysiwyg or markdown)
-// - hideModeSwitch (하단 바 숨김 여부)
-// - toolbarItems (툴바 옵션)
-// - dark theme (다크 테마 적용)
-// - useCommandShortcut (키보드 입력 컨트롤 방지)
-// - usageStatistics (통계 수집)
