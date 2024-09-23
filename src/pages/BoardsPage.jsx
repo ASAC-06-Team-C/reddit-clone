a37bd66c674889686978202ac40581d13b8cadcb
Seok-Hun <hamer1233@naver.com>
2024-09-23 19:47:30 +0900

    변경 사항 - main의 라우팅 변경 - 페이지 폴더에 각 페이지 파일 만들어 사용 - Layout.jsx 변경

---
+++ src/pages/BoardsPage.jsx
@@ -0,0 +1,13 @@
+import Header from '@/components/header'
+import Post from '@/components/Post'
+import RedditFirstAttachPage from '@/components/RedditFirstAttach'
+
+function BoardsPage() {
+  return (
+    <>
+      <RedditFirstAttachPage />
+    </>
+  )
+}
+
+export default BoardsPage
