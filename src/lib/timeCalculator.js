abfd9fd9840866f550c61daaeb8bedab9ad54c93
Seok-Hun <hamer1233@naver.com>
2024-10-31 21:35:19 +0900

    Util : 현재 시간에서 이전 시간 빼는 timeCalculator 구현 반환 값 예시 : 10초 전, 2일 전, 방금 전 등

---
+++ src/lib/timeCalculator.js
@@ -0,0 +1,38 @@
+export const calculator = (pastTime) => {
+  const past = new Date(pastTime).getMilliseconds()
+  const now = new Date().getMilliseconds()
+  const timeDivideSet = [
+    {
+      divide: 1000,
+      type: '초',
+    },
+    {
+      divide: 1000 * 60,
+      type: '분',
+    },
+    {
+      divide: 1000 * 60 * 60,
+      type: '시간',
+    },
+    {
+      divide: 1000 * 60 * 60 * 24,
+      type: '일',
+    },
+    {
+      divide: 1000 * 60 * 60 * 24 * 30,
+      type: '달',
+    },
+    {
+      divide: 1000 * 60 * 60 * 24 * 30 * 365,
+      type: '년',
+    },
+  ]
+
+  for (const { divide, type } of timeDivideSet.reverse()) {
+    const result = Math.trunc((now - past) / divide)
+    if (result > 0) {
+      return `${result}${type} 전`
+    }
+  }
+  return '방금 전'
+}
