dab26f51e01a45ac59f83af409fe98c039aa2667
Seok-Hun <hamer1233@naver.com>
2024-10-31 11:13:07 +0900

    Feat: 시간 소수점 자르기

--- src/components/BoardDetail.jsx
+++ src/components/BoardDetail.jsx
@@ -25,7 +25,6 @@ import IconTextButton from '@/components/IconTextButton'
 import { useParams } from 'react-router-dom'
 
 function BoardDetail() {
-  console.log('render')
   const url = 'http://localhost:8080/posts/'
   const params = useParams()
 
@@ -47,80 +46,82 @@ function BoardDetail() {
   }, [])
 
   useEffect(() => {
-    console.log('setDate')
     if (content) {
       setDiffDate(
-        (new Date().getTime() - new Date(content.post_write_date).getTime()) /
-          (24 * 60 * 60 * 1000),
+        Math.floor(
+          ((new Date().getTime() - new Date(content.post_write_date).getTime()) /
+            (24 * 60 * 60 * 1000)) *
+            100,
+        ) / 100,
       )
     }
   }, [content])
 
-  // if (loading) {
-  //   return (
-  //     <div>
-  //       <h1>로딩중입니다.</h1>
-  //     </div>
-  //   )
-  // } else {
-  return (
-    <>
-      <Card className='w-full'>
-        <CardHeader>
-          <div className='flex justify-between'>
-            <div className='flex items-center gap-2'>
-              <Avatar>
-                <AvatarImage src={'img/blank-profile-picture-973460_960_720.webp'} />
-                <AvatarFallback>KOR</AvatarFallback>
-              </Avatar>
-              {/* <div>{content.author.user_no}</div> */}
-              <div>•</div>
-              <div>{diffDate}일 전</div>
+  if (loading) {
+    return (
+      <div>
+        <h1>로딩중입니다.</h1>
+      </div>
+    )
+  } else {
+    return (
+      <>
+        <Card className='w-full'>
+          <CardHeader>
+            <div className='flex justify-between'>
+              <div className='flex items-center gap-2'>
+                <Avatar>
+                  <AvatarImage src={'/img/blank-profile-picture-973460_960_720.webp'} />
+                  <AvatarFallback>KOR</AvatarFallback>
+                </Avatar>
+                {/* <div>{content.author.user_no}</div> */}
+                <div>•</div>
+                <div>{diffDate}일 전</div>
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button size='icon' variant='ghost'>
+                    <img src={'/img/three-dots.svg'} className='h-4 w-4'></img>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>Save</DropdownMenuItem>
+                  <DropdownMenuItem>Hide</DropdownMenuItem>
+                  <DropdownMenuItem>Report</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button size='icon' variant='ghost'>
-                  <img src={'/img/three-dots.svg'} className='h-4 w-4'></img>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>Save</DropdownMenuItem>
-                <DropdownMenuItem>Hide</DropdownMenuItem>
-                <DropdownMenuItem>Report</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </CardHeader>
-        <CardContent>
-          <div>{content.post_title}</div>
-          <div
-            dangerouslySetInnerHTML={{ __html: marked.parse(content.post_content) }}
-            className='prose'
-          ></div>
-        </CardContent>
-        <CardFooter>
-          <div className='flex gap-2'>
-            <div className='rounded-full bg-gray-200'>
-              <IconButton variant='ghost' iconSrc={'/img/up-arrow.svg'} onClickEvent={() => {}} />
-              {content.post_vote_count}
-              <IconButton
-                variant='ghost'
-                iconSrc={'/img/up-arrow-svgrepo-com.svg'}
-                onClickEvent={() => {}}
+          </CardHeader>
+          <CardContent>
+            <div>{content.post_title}</div>
+            <div
+              dangerouslySetInnerHTML={{ __html: marked.parse(content.post_content) }}
+              className='prose'
+            ></div>
+          </CardContent>
+          <CardFooter>
+            <div className='flex gap-2'>
+              <div className='rounded-full bg-gray-200'>
+                <IconButton variant='ghost' iconSrc={'/img/up-arrow.svg'} onClickEvent={() => {}} />
+                {content.post_vote_count}
+                <IconButton
+                  variant='ghost'
+                  iconSrc={'/img/up-arrow-svgrepo-com.svg'}
+                  onClickEvent={() => {}}
+                />
+              </div>
+              <IconTextButton
+                variant='secondary'
+                iconSrc={'/img/306434.svg'}
+                text={content.post_comment_count}
               />
+              <IconTextButton variant='secondary' iconSrc={'/img/share-arrows.svg'} text='Share' />
             </div>
-            <IconTextButton
-              variant='secondary'
-              iconSrc={'/img/306434.svg'}
-              text={content.post_comment_count}
-            />
-            <IconTextButton variant='secondary' iconSrc={'/img/share-arrows.svg'} text='Share' />
-          </div>
-        </CardFooter>
-      </Card>
-    </>
-  )
+          </CardFooter>
+        </Card>
+      </>
+    )
+  }
 }
-// }
 
 export default BoardDetail
